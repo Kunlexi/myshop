6c3dd6d9c1c5b6f1738795fee919c59d51b2e0dd
kunlexi <adesinataofik5@gmail.com>
2023-02-06 23:08:55 +0100

    commit

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -2,59 +2,87 @@ import { useState } from "react";
 import styles from "./auth.module.scss";
 import registerImg from "../../assets/register.png";
 import Card from "../../components/card/Card";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase/config";
+import Loader from "../../components/loader/Loader";
 
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [cPassword, setCPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
 
   const registerUser = (e) => {
-    e.preventDefault()
-    console.log(email, password, cPassword)
-  }
-  return (
-    <section className={`container ${styles.auth}`}>
-      <Card>
-        <div className={styles.form}>
-          <h2>Register</h2>
+    e.preventDefault();
+    if (password !== cPassword) {
+      toast.error("Passwords do not match.");
+    }
+    setIsLoading(true);
 
-          <form onSubmit={registerUser}>
-            <input
-              type="text"
-              placeholder="Email"
-              required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              required
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <input
-              type="Password"
-              placeholder="Confirm Password"
-              required
-              value={cPassword}
-              onChange={(e) => setCPassword(e.target.value)}
-            />
-            <button type="submit" className="--btn --btn-primary --btn-block">
-              Register
-            </button>
-          </form>
-          <span className={styles.register}>
-            <p>Already have an account?</p>
-            <Link to="/login">Login</Link>
-          </span>
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        setIsLoading(false);
+        toast.success("Registration Successful...");
+        navigate("/login");
+      })
+      .catch((error) => {
+        toast.error(error.message);
+        setIsLoading(false);
+      });
+  };
+  return (
+    <>
+      <ToastContainer />
+      {isLoading && <Loader />}
+      <section className={`container ${styles.auth}`}>
+        <Card>
+          <div className={styles.form}>
+            <h2>Register</h2>
+
+            <form onSubmit={registerUser}>
+              <input
+                type="text"
+                placeholder="Email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <input
+                type="Password"
+                placeholder="Confirm Password"
+                required
+                value={cPassword}
+                onChange={(e) => setCPassword(e.target.value)}
+              />
+              <button type="submit" className="--btn --btn-primary --btn-block">
+                Register
+              </button>
+            </form>
+            <span className={styles.register}>
+              <p>Already have an account?</p>
+              <Link to="/login">Login</Link>
+            </span>
+          </div>
+        </Card>
+        <div className={styles.img}>
+          <img src={registerImg} alt="register" width="400" />
         </div>
-      </Card>
-      <div className={styles.img}>
-        <img src={registerImg} alt="register" width="400" />
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
