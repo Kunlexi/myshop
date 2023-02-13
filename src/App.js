793793e14cd60f64654a89d19bd856aa90aff0a2
kunlexi <adesinataofik5@gmail.com>
2023-02-13 14:30:10 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,33 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 //pages
-import { Home, Contact, Login, Register, Reset } from "./pages";
+import { Home, Contact, Login, Register, Reset, Admin } from "./pages";
 // components
-import { Header, Footer } from "./components"
-
+import { Header, Footer } from "./components";
+import AdminOnlyRoute from "./components/adminOnlyRoute/AdminOnlyRoute";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-      <ToastContainer />
+        <ToastContainer />
         <Header />
-          <Routes>
-            <Route path="/" element={ <Home/> }/>
-            <Route path="/contact" element={ <Contact/> }/>
-            <Route path="/login" element={ <Login/> }/>
-            <Route path="/register" element={ <Register/> }/>
-            <Route path="/reset" element={ <Reset/> }/>
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/reset" element={<Reset />} />
+
+          <Route
+            path="/admin/*"
+            element={
+              <AdminOnlyRoute>
+                <Admin />
+              </AdminOnlyRoute>
+            }
+          />
+        </Routes>
         <Footer />
       </BrowserRouter>
     </>
