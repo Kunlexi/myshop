2f19fef73f6a0676fac3f320c70c991d26bcf1e9
kunlexi <adesinataofik5@gmail.com>
2023-02-16 15:27:58 +0100

    viewproducts

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -1,7 +1,44 @@
-import React from "react";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { db } from "../../../firebase/config";
+import styles from "./ViewProducts.module.scss";
 
 const ViewProducts = () => {
-  return <div>ViewProducts</div>;
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = () => {
+    setIsLoading(true);
+
+    try {
+      const productsRef = collection(db, "products");
+      const q = query(productsRef, orderBy("createdAt", "desc"));
+
+      onSnapshot(q, (snapshot) => {
+        // console.log(snapshot.docs);
+        const allProducts = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        console.log(allProducts);
+        setProducts(allProducts);
+      });
+    } catch (error) {
+      setIsLoading(false);
+      toast.error(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h1>ViewProducts</h1>
+    </div>
+  );
 };
 
 export default ViewProducts;
